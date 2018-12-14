a0b87395b520980983dd71c455603dddb646ebc0
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-12-14 23:57:14 +0800

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <set>
+#include <iterator>
+#include "Node.h"
+
+typedef std::set<Node*>::iterator node_iterator;
+
+class Graph
+{
+	std::set<Node*> nodes;
+public:
+	Graph() {};
+	Graph(const char* inFileName);
+	int sizeG() const;
+	void kill();
+	void addNode(Node* node);
+	void removeNode(Node* node);
+	void addEdge(Node* begin, Node* end);
+	void removeEdge(Node* begin, Node* end);
+	bool ser(Node* nd);
+	node_iterator begin() const
+	{
+		return nodes.begin();
+	}
+	node_iterator end() const
+	{
+		return nodes.end();
+	}
+};
