f743a65e68e49394f8cfa616d6507a5f606649ea
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-12-14 23:58:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
 	Graph one = Graph();
 	BFS n = BFS(g);
 	int i = 1;
-	/*one.addNode(*g.begin());
+	one.addNode(*g.begin());
 
 	for (node_iterator it = g.begin();it != g.end();it++)
 		for (node_iterator jt = g.begin(); jt != g.end(); jt++)
@@ -39,35 +39,7 @@ int main()
 	cout << endl << "Two: ";
 	for (node_iterator it = g.begin(); it != g.end(); it++)
 		cout << (*it)->getName() << ' ';
-	cout << endl;*/
-
-	while (g.sizeG() != 0)
-	{
-		node_iterator it = g.begin();
-		one.addNode(*it);
-		for (node_iterator jt = g.begin(); jt != g.end(); jt++)
-		{
-			if (it == jt)
-				continue;
-			if (one.ser(*jt))
-				continue;
-			n.print(*it, *jt, one);
-		}
-
-		for (node_iterator it = g.begin(); it != g.end(); it++)
-			if (one.ser(*it))
-				g.removeNode(*it);
-
-		cout << i << ': ';
-
-		for (node_iterator it = one.begin(); it != one.end(); it++)
-			cout << (*it)->getName() << ' ';
-		
-		cout << endl;
-
-		i++;
-		one.kill();
-	}
+	cout << endl;
 
 	system("pause");
 	return 0;
