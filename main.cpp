87570df8ab413ca6e417fa24a13338bf77883b6c
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-12-14 23:58:10 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <locale.h>
+#include "Graph.h"
+#include "Node.h"
+#include "FS.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const char* inFileName = "in.txt";//argv[0];
+	Graph g = Graph(inFileName);
+	Graph one = Graph();
+	BFS n = BFS(g);
+	int i = 1;
+	/*one.addNode(*g.begin());
+
+	for (node_iterator it = g.begin();it != g.end();it++)
+		for (node_iterator jt = g.begin(); jt != g.end(); jt++)
+		{
+			if (it == jt)
+				continue;
+			if (one.ser(*jt))
+				continue;
+			n.print(*it, *jt, one);
+		}
+
+	cout << "One: ";
+	for (node_iterator it = g.begin(); it != g.end(); it++)
+		if (one.ser(*it))
+			g.removeNode(*it);
+
+	for (node_iterator it = one.begin(); it != one.end(); it++)
+		cout << (*it)->getName() << ' ';
+
+	cout << endl << "Two: ";
+	for (node_iterator it = g.begin(); it != g.end(); it++)
+		cout << (*it)->getName() << ' ';
+	cout << endl;*/
+
+	while (g.sizeG() != 0)
+	{
+		node_iterator it = g.begin();
+		one.addNode(*it);
+		for (node_iterator jt = g.begin(); jt != g.end(); jt++)
+		{
+			if (it == jt)
+				continue;
+			if (one.ser(*jt))
+				continue;
+			n.print(*it, *jt, one);
+		}
+
+		for (node_iterator it = g.begin(); it != g.end(); it++)
+			if (one.ser(*it))
+				g.removeNode(*it);
+
+		cout << i << ': ';
+
+		for (node_iterator it = one.begin(); it != one.end(); it++)
+			cout << (*it)->getName() << ' ';
+		
+		cout << endl;
+
+		i++;
+		one.kill();
+	}
+
+	system("pause");
+	return 0;
+}
